1d5e3720801190a19905566868740d43c94b16eb
Zanata Mahatma <zanatamahatma@gmail.com>
2021-06-17 22:57:16 -0300

    Correção da calculadora

---
+++ ex037.py
@@ -0,0 +1,17 @@
+'''Exercício Python 37: Escreva um programa em Python que leia um número inteiro qualquer e
+peça para o usuário escolher qual será a base de conversão: 1 para binário, 2 para octal e 3 para hexadecimal.'''
+
+num = int(input('Digite um número inteiro: '))
+print('''Escolha uma das bases para conversão:
+[ 1 ] converter para BINÁRIO
+[ 2 ] converter para OCTAL
+[ 3 ] converter para HEXADECIMAL''')
+opção = int(input('Sua Opção: '))
+if opção == 1:
+    print('{} convertido para BINÁRIO é igual a {}'.format(num,bin(num)[2:]))
+elif opção == 2:
+    print('{} convertido para OCTAL é igual a {}'.format(num,oct(num)[2:]))
+elif opção == 3:
+    print('{} convertido para HEXADECIMAL é igual a {}'.format(num,hex(num)[2:]))
+else:
+    print('Opção inválida. Tente novamente')
