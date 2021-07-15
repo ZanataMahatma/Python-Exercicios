de34ba161c17543f2b271d30c07747d7f0fbb663
Zanata Mahatma <zanatamahatma@gmail.com>
2021-07-15 00:24:36 -0300

    correção e ajuste  nas pastas para consulta rapidas

---
+++ Modularização em Python/aula22.py
@@ -0,0 +1,6 @@
+from uteis import numeros
+
+num = int(input('Digite um Valor: '))
+fat = numeros.fatorial(num)
+print(f'O fatorial de {num} é {fat}')
+print(f'O dobro de {num} é o {numeros.dobro(num)}')
